b90b0995b9e738f8b65851feb55cd4b62cbd7c71
Hashisx <rmiller805@gmail.com>
2015-01-21 17:38:47 -0800

    master

---
+++ login.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<!--
+Designed by: http://www.cssing.org
+Released for free under a Creative Commons Attribution 3.0 License: http://creativecommons.org/licenses/by/3.0/
+Name: Birchwood
+Description:  A two-columns, responsive design template.
+Template number: 19
+Version: 1.0
+Released: 4.3.13
+-->
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Just a Backpack</title>
+<link href='http://fonts.googleapis.com/css?family=Cutive' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Stoke' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Fredericka+the+Great' rel='stylesheet' type='text/css'>
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="wrapper">
+	<header>
+    	<h1><a href="">Just a Backpack</a></h1>
+        <nav>
+        	<ul>
+            	<li id="active"><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="news.php">News</a></li>
+                <li><a href="forums.php">Forums</a></li>
+                <li><a href="register.php">Register</a></li>>
+            </ul>
+        </nav>
+        <div class="clearfloat"></div>
+        <hr>
+        <div class="headerPicContainer">
+            
+            <div class="websiteDescription">
+				
+                <h2>Registration Instructions</h2>
+				<p>1</p>
+				<p>2</p>
+				<p>3</p>
+				<p>4</p>
+				<p>5</p>
+				<p>6</p>
+				<p>7</p>
+				<p>8</p>
+				<p>a</p>
+				<p>b</p>
+				<p>c</p>
+				<p>d</p>
+				<p>a</p>
+				<p>b</p>
+				<p>c</p>
+				<p>d</p>
+				<p>a</p>
+				<p>b</p>
+				<p>c</p>
+				<p>d</p>
+                
+            </div>
+            <div class="clearfloat"></div>
+        </div>
+        <div class="clearfloat"></div>
+    </header>
+
+    <div class="clearfloat"></div>
+    <footer>
+        <ul>
+            <li>
+                <span>Your Company Name</span>
+            </li>
+            <li>
+                Designed by <a href="http://www.cssing.org" title="Free Css Templates">Free Css Templates</a>
+            </li>
+            <li>
+                Validation 
+                <a class="footerLink" href="http://validator.w3.org/check/referer" title="This page validates as HTML5"><abbr title="HyperText Markup Language">HTML5</abbr></a>, 
+                <a class="footerLink" href="http://jigsaw.w3.org/css-validator/check/referer" title="This page validates as CSS"><abbr title="Cascading Style Sheets">CSS3</abbr></a>
+            </li>
+        </ul>
+    </footer>
+</div>
+</body>
+</html>
