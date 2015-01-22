712bcf6c34ff09016dd13d6010bb3218c61aa1c2
Hashisx <rmiller805@gmail.com>
2015-01-22 09:30:59 -0800

    master

---
+++ register.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Admin - Add User</title>
+  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="../style/main.css">
+</head>
+<body>
+	<form action='' method='post'>
+
+		<p><label>Username</label><br />
+		<input type='text' name='username' value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
+
+		<p><label>Password</label><br />
+		<input type='password' name='password' value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
+
+		<p><label>Confirm Password</label><br />
+		<input type='password' name='passwordConfirm' value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
+
+		<p><label>Email</label><br />
+		<input type='text' name='email' value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
+		
+		<p><input type='submit' name='submit' value='Add User'></p>
+
+	</form>
+<div id="wrapper">
+	<?php
+
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+
+		//collect form data
+		extract($_POST);
+
+		//very basic validation
+		if($username ==''){
+			$error[] = 'Please enter the username.';
+		}
+
+		if($password ==''){
+			$error[] = 'Please enter the password.';
+		}
+
+		if($passwordConfirm ==''){
+			$error[] = 'Please confirm the password.';
+		}
+
+		if($password != $passwordConfirm){
+			$error[] = 'Passwords do not match.';
+		}
+
+		if($email ==''){
+			$error[] = 'Please enter the email address.';
+		}
+
+		if(!isset($error)){
+
+
+
+			try {
+
+				define('IN_PHPBB', true);
+$phpbb_root_path = 'forum/';
+
+$phpEx = substr(strrchr(_FILE_, '.'), 1);
+include($phpbb_root_path . 'common.php');
+include($phpbb_root_path . 'includes/functions_user.php');
+include($phpbb_root_path . 'includes/ucp/ucp_register.php');
+	$sql_ary = array(
+        ':username'          => $username,
+        ':username_clean'    => $username_clean,
+        ':user_password'     => phpbb_hash($password),
+        ':user_email'        => $user_email,
+        ':user_email_hash'   => crc32(strtolower($user_email)) . strlen($user_email),
+        ':group_id'          => 2,
+        ':user_type'         => 0,
+    );
+user_add($sql_ary);
+
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}
+
+		}
+
+	}
+
+	//check for any errors
+	if(isset($error)){
+		foreach($error as $error){
+			echo '<p class="error">'.$error.'</p>';
+		}
+	}
+	?>
+
+
+
+</div>
