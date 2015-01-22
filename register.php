ac2bc4e6de10e0ec93ac89cab8a7e7c795928306
Hashisx <rmiller805@gmail.com>
2015-01-21 20:16:35 -0800

    master

--- register.php
+++ register.php
@@ -40,7 +40,60 @@ Released: 4.3.13
 
 
 <h2>Registration Instructions</h2>
+
+	<form action='' method='post'>
+
+		<p><label>Username</label><br />
+		<input type='text' name='username' value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
+
+		<p><label>Password</label><br />
+		<input type='password' name='password' value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
+
+		<p><label>Confirm Password</label><br />
+		<input type='password' name='passwordConfirm' value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
+
+		<p><label>Email</label><br />
+		<input type='text' name='email' value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
+		
+		<p><input type='submit' name='submit' value='Add User'></p>
+
+	</form>
+
 <?php
+
+
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+
+		//collect form data
+		extract($_POST);
+
+		//very basic validation
+		if($username ==''){
+			$error[] = 'Please enter the username.';
+		}
+
+		if($password ==''){
+			$error[] = 'Please enter the password.';
+		}
+
+		if($passwordConfirm ==''){
+			$error[] = 'Please confirm the password.';
+		}
+
+		if($password != $passwordConfirm){
+			$error[] = 'Passwords do not match.';
+		}
+
+		if($email ==''){
+			$error[] = 'Please enter the email address.';
+		}
+
+		if(!isset($error)){
+
+			$hashedpassword = $user->create_hash($password);
+
+
 define('IN_PHPBB', true);
 $phpbb_root_path = 'forum/';
 
@@ -48,13 +101,7 @@ $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.php');
 include($phpbb_root_path . 'includes/functions_user.php');
 include($phpbb_root_path . 'includes/ucp/ucp_register.php');
-
-$password='pword';
-$username='uname';
-$username_clean=strtolower($username);
-$user_email='email@email.net';
-
-    $sql_ary = array(
+	$sql_ary = array(
         'username'          => $username,
         'username_clean'    => $username_clean,
         'user_password'     => phpbb_hash($password),
