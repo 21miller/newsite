663cd4612657252ad9104d376ffda044244917b9
Hashisx <rmiller805@gmail.com>
2015-01-26 13:38:08 -0800

    c

--- register.php
+++ register.php
@@ -1,18 +1,15 @@
-<?php
+<?php 
+   //phpbb3 login, user session management
+   $phpbb_root_path = './vernon_forum/';
+   $returnAddress ='../index.php';
+   //including phpBB3 stuff   
+   define('IN_PHPBB', true);
+   $phpEx = substr(strrchr(__FILE__, '.'), 1);
+   include($phpbb_root_path . 'common.' . $phpEx);
+   include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
 
-define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : 'forum/';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-
-
-if (!$user->data['is_registered'])
-{
-    if ($user->data['is_bot'])
-    {
-        // the user is a bot, send them back to home plate...
-        redirect(append_sid("{$phpbb_root_path}index.$phpEx"));
-    }
-    // the user is not logged in, give them a chance to login here...
-    login_box('', 'LOGIN');
-} 
+   // Start phpbb3 session management
+   $user->session_begin();
+   $auth->acl($user->data);
+   $user->setup('viewforum');
 ?>
